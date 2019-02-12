753a56b02b352f889eb6335a96308096b0994a60
Julie Pham <tuyetnganphamhoang@gmail.com>
2019-02-12 20:19:47 +0700

    add count time

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
 });
-
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function() {
+    console.log('user disconnected');
+  });
+  socket.on('chat message', function(msg) {
+    console.log('message: ' + msg);
+    io.emit('chat message', msg);
+  });
+  socket.on('ring bell', function(msg) {
+    console.log('message: ' + msg);
+    io.emit('ring bell', msg);
+  });
+  socket.on('count time', function(msg) {
+    console.log('message: ' + msg);
+    io.emit('count time', msg);
+    if (msg == 0) {
+      io.emit('stop');
+    }
+  });
+  socket.broadcast.emit('hi');
+});
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
