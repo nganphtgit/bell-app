e484d6b741e93fa626428fda59bafc14ab4b9312
Ngân <tuyetnganphamhoang@gmail.com>
2019-02-17 00:29:03 +0700

    add configuration for node prj

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+import express from 'exrpress';
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
