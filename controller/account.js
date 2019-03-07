720f13e310a7587b20a942467f35c2129dd54e16
Ngân <tuyetnganphamhoang@gmail.com>
2019-03-07 09:49:37 +0700

    remove config mvc

--- controller/account.js
+++
@@ -1,7 +0,0 @@
-import mongoose from 'mongoose';
-import {Router} from 'express';
-import Account from '../model/account';
-import bodyParser from 'body-parser';
-import passport from 'passport';
-
-
